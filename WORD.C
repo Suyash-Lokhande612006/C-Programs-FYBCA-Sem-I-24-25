195b3d5723da7d3c1b40cf8f463e2560689aa74d
Suyash-Lokhande612006 <lokhandesuyash66@gmail.com>
2025-01-11 10:15:03 +0530

    Add files via upload

---
+++ WORD.C
@@ -0,0 +1,35 @@
+/*To Convert Number into Word using switch-case Statement*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int N;
+	clrscr();
+	printf("Enter any Number\n");
+	scanf("%d",&N);
+	switch(N)
+	{
+		case 0:	printf("Zero\n");
+			break;
+		case 1: printf("One\n");
+			break;
+		case 2:	printf("Two\n");
+			break;
+		case 3:	printf("Three\n");
+			break;
+		case 4:	printf("Four\n");
+			break;
+		case 5:	printf("Five\n");
+			break;
+		case 6:	printf("Six\n");
+			break;
+		case 7:	printf("Seven\n");
+			break;
+		case 8:	printf("Eight\n");
+			break;
+		case 9:	printf("Nine\n");
+			break;
+		default: printf("Wrong Choice\n");
+	}
+	getch();
+}
